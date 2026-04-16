cbf1763d6873ba2a34ceca237a20c088bb5ad619
Your Name <nageshramagond14@gmail.com>
2026-04-16 11:53:42 +0530

    react.js-basics

---
+++ project2/script.js
@@ -0,0 +1,9 @@
+function calculateGST(price){
+    return price *0.18;
+}
+
+document.getElementById("btn").addEventListener("click",function(){
+    let price =document.getElementById("price").value;
+    let gst = calculateGST(price);
+    document.getElementById("result").innerText="Total price is:"+ (Number(price)+ Number(gst));
+});
